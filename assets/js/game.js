709cc48a3a9f8e1522e99c52344bc159ff0fe53e
Margaret Hansen <margareh@andrew.cmu.edu>
2025-08-07 22:50:56 -0700

    Randomize questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,21 @@ var q;
 var game_init = false;
 
 // randomize data
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
 
 // Start the game
 function start() {
@@ -22,6 +37,7 @@ function start() {
     d = document.getElementById("game");
     d.classList.add("game-play");
     d.classList.remove("game-start");
+    shuffle(rows);
     game_init = true;
 
 }
